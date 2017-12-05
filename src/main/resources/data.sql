2790956c5c26b974054022994a2cddf37b6350e7
TanmayTapar <tanmaytapar@gmail.com>
2017-12-05 18:03:39 -0500

    Added Script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Product (product_name,quantity) VALUES ('Iphone',20);
+INSERT INTO Product (product_name,quantity) VALUES ('Pixel2',10);
+
+INSERT INTO Users (name,email,phone_number,address) VALUES ('Ted','ted@xyz.com','123123','Dulles');
+INSERT INTO Users (name,email,phone_number,address) VALUES ('Barney','barney@xyz.com','3453123','New York');
+
+
+
